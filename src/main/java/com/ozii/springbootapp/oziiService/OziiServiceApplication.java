82bcd0ab43ee4184c37b9a3a429e4a8c894a9994
Krzysztof Szewczyk <k.l.szewczyk@stud.elka.pw.edu.pl>
2019-02-13 18:49:04 +0100

    first spring commit

---
+++ src/main/java/com/ozii/springbootapp/oziiService/OziiServiceApplication.java
@@ -0,0 +1,14 @@
+package com.ozii.springbootapp.oziiService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OziiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OziiServiceApplication.class, args);
+	}
+
+}
+
