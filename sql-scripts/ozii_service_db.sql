0d58bc833dd5631fbd6bae71f6c4f3753524f941
Krzysztof Szewczyk <k.l.szewczyk@stud.elka.pw.edu.pl>
2019-02-13 22:21:08 +0100

    SQL scripts, index.html, home-page.html

---
+++ sql-scripts/ozii_service_db.sql
@@ -0,0 +1,35 @@
+CREATE DATABASE IF NOT EXISTS `ozii_Service`;
+USE `ozii_Service`;
+
+DROP TABLE IF EXISTS `player`;
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user`(
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+    `first_name` varchar(45) DEFAULT NULL,
+    `last_name` varchar(45) DEFAULT NULL,
+    `age` int(3) default null,
+    `nick` varchar(45) default null,
+     primary key (`id`),
+     unique key `nick_unique` (`nick`)
+)ENGINE=InnoDB auto_increment=1 default charset=utf8;
+
+
+create table `player`(
+	`id` int(11) not null auto_increment,
+    `score` int(11) default null,
+    `user_id` int(11) default null,
+    primary key(`id`),
+    key `FK_user_id` (`user_id`),
+    constraint `FK_user_id` foreign key (`user_id`) references `user` (`id`)
+)ENGINE=InnoDB auto_increment=1 default charset=utf8;
+
+insert into `user` values
+(1, 'Chris', 'Shoemaker', 22, 'monster1'),
+(2, 'Kate', 'Ziomkowska', 16, 'fighter123'),
+(3, 'Janusz', 'Kowalski', 40, 'snowMoon');
+
+insert into `player` values
+(1, 101, 2),
+(2, 1000, 1),
+(3, 255, 3);
--- src/main/java/com/ozii/springbootapp/oziiService/controller/UserController.java
+++ src/main/java/com/ozii/springbootapp/oziiService/controller/UserController.java
@@ -1,15 +1,17 @@
 package com.ozii.springbootapp.oziiService.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("home")
+@RequestMapping("/")
 public class UserController {
 	
+	@GetMapping("/home")
 	public String login() {
 		
-		return "";
+		return "/home-page";
 	}
 	
 }
